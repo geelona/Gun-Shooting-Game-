0bddba9cc36b0700119af4ab7da5a2cb9168c834
geelona <mr.geelona@gmail.com>
2022-07-27 15:59:02 +0300

    added images

--- screen.py
+++ screen.py
@@ -3,5 +3,5 @@ import pygame
 W, H = 800, 600
 
 win = pygame.display.set_mode((W, H))
-
+pygame.display.set_icon(pygame.image.load('images/EvilUkranianEgg.png'))
 pygame.display.set_caption("Gun Shooting")
