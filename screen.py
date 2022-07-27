329e7b8305c51864263bd4e1aed1468696d9fde3
geelona <mr.geelona@gmail.com>
2022-07-27 16:01:04 +0300

    fix mistake with import egg images

--- screen.py
+++ screen.py
@@ -3,5 +3,6 @@ import pygame
 W, H = 800, 600
 
 win = pygame.display.set_mode((W, H))
-pygame.display.set_icon(pygame.image.load('images/EvilUkranianEgg.png'))
+egg = pygame.image.load('images/EvilUkranianEgg.png')
+pygame.display.set_icon(pygame.image.load('images/EvilUkrainianEgg.png'))
 pygame.display.set_caption("Gun Shooting")
